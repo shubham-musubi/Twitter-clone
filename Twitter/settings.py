c96335ae5e4acc1a71154ce3d6c0d1c969bf0cf7
shubham-musubi <71789009+shubham-musubi@users.noreply.github.com>
2021-06-06 18:34:36 +0530

    Update settings.py

--- Twitter/settings.py
+++ Twitter/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 cloudinary.config( 
   cloud_name = "dumu78x6g", 
